169815c17b58492a02236a37b166ffcd8ae19218
Alejandro Redondo Fernández <alexredfdez1@gmail.com>
2025-02-07 09:51:04 +0100

    Last changes

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Alejandro Redondo");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("IES San Andrés");
+    System.out.println("León, España");
   }
   
 }
